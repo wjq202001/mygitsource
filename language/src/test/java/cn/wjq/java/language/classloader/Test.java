3adc557cf55581e3eb5450105f70053b8162d36e
wjq202001 <wjq202001@hotmail.com>
2018-04-01 22:42:31 +0800

    classloader

--- language/src/test/java/cn/wjq/java/language/classloader/Test.java
+++ language/src/test/java/cn/wjq/java/language/classloader/Test.java
@@ -1,8 +1,11 @@
 package cn.wjq.java.language.classloader;
 
+import org.junit.Assert;
+
 import java.io.File;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
@@ -23,5 +26,22 @@ public class Test {
         Constructor constr = c.getDeclaredConstructor(pTypes);
         Object obj = constr.newInstance(new Object[0]);
 
+    }
+
+    @org.junit.Test
+    public void test2() throws MalformedURLException, NoSuchMethodException, InvocationTargetException, IllegalAccessException, ClassNotFoundException {
+        File file = new File("D:\\Git\\libs\\simpleHttp-1.0.0.0.jar");
+        URL url = file.toURI().toURL();
+        ClassLoader classLoader = (URLClassLoader) ClassLoader.getSystemClassLoader();
+
+        Method addUrlMethod = URLClassLoader.class.getDeclaredMethod("addURL", new Class[]{URL.class});
+        addUrlMethod.setAccessible(true);
+        addUrlMethod.invoke(classLoader, new Object[]{url});
+
+        Class c1 = Class.forName("simpleHttp.extentions.NoCache");
+        Assert.assertNotNull(c1);
+
+        Class c2 = classLoader.loadClass("simpleHttp.extentions.NoCache");
+        Assert.assertNotNull(c2);
     }
 }
